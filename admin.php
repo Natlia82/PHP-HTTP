245127460b3f5e0b7fb86c1f885ad5e89043d4db
Natlia82 <43696028+Natlia82@users.noreply.github.com>
2019-01-22 19:08:13 +0500

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <title>Обработка форм</title>
+  <meta charset="utf-8">
+  <link type="text/css" href="css/style.css" rel="stylesheet">
+</head>
+<body>
+  <form enctype="multipart/form-data" action="" method="POST">
+    <div>Выбирите файл с тестом</div>
+     <br>
+       <input name="userfile" type="file" />
+     <br>
+     <br>
+   <input type="submit" value="Отправить">
+  </form>
+  <?php
+  if (!empty($_FILES)) {
+
+    if (array_key_exists('userfile', $_FILES)) {
+      if ($_FILES['userfile']['error'] === 0) {
+        $name = basename($_FILES["userfile"]["name"]);
+        if (move_uploaded_file($_FILES['userfile']['tmp_name'], "temp/$name"))
+        {
+           //  echo "файл сохранен";
+          echo '<meta http-equiv="refresh" content="0;URL=/list.php">';
+        }
+
+
+
+      }
+    }
+  }
+   ?>
+  <br>
+  <br>
+  <a href="list.php">lest.php</a>
+</body>
+</html>
---
+++ list.php
@@ -0,0 +1,30 @@
+<?php
+
+  $directory = 'c:\server\domains\localhost\temp/';
+  $scandir = scandir($directory, 1);
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <title>Обработка форм</title>
+  <meta charset="utf-8">
+  <link type="text/css" href="css/style.css" rel="stylesheet">
+</head>
+  <body>
+    <ul>
+      <?php
+        $ra = null;
+        foreach ($scandir as $value) {
+          $ra = substr($value, -4);
+          if ($ra === 'JSON' or $ra === 'json') {
+            if ($value != '.' and $value != '..') {
+            ?>
+            <li><a href="test.php?test=<?php echo $value; ?>" ><?php echo $value; ?></a></li>
+            <?php
+            }
+          }
+        } ?>
+    </ul>
+  </body>
+</html>
